570c1f011ea5ae0e8623f89b6615ec3e5187ac42
qshal567 <147568122+qshal567@users.noreply.github.com>
2024-02-10 19:05:06 -0500

    Update entry point sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 echo "Apply Database Migration"
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
-gunicorn src.wsgi:application --bind 0.0.0.0:8000
+gunicorn portfolio.wsgi:application --bind 0.0.0.0:8000
