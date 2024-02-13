f7da22e0283b49a656118c9223a37d76a8c9a9df
qshal567 <147568122+qshal567@users.noreply.github.com>
2024-02-12 21:39:35 -0500

    updated static files

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -126,7 +126,7 @@ MEDIA_ROOT= BASE_DIR /'media'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / 'home' / 'static'
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
